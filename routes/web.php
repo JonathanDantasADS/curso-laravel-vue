e3392b945e6e7066684a058d1f63526cd298cf27
JonathanDantasADS <jonathandantasadm@gmail.com>
2022-07-02 02:27:56 -0300

    redirecionamento_rotas

--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,39 @@ use Illuminate\Support\Facades\Route;
 |
 */
     
-    Route::get('/', 'PrincipalController@principal');
-    Route::get('/sobre-nos', 'SobreNosController@sobreNos');
-    Route::get('/contato', 'ContatoController@contato');
-    // Nome, categoria, assunto, mensagem
+    Route::get('/', 'PrincipalController@principal')->name('site.index');
+    Route::get('/sobre-nos', 'SobreNosController@sobreNos')->name('site.sobrenos');
+    Route::get('/contato', 'ContatoController@contato')->name('site.contato');
+    
+    Route::get('/login', function(){
+        return 'Login';
+    })->name('site.login');
+
+    // Rotas agrupadas
+    Route::prefix('/app')->group(function(){
+
+        Route::get('/clientes', function(){
+            return 'Clientes';
+        })->name('app.clientes');
+        
+        Route::get('/fornecedores', function(){
+            return 'Fornecedores';
+        })->name('app.fornecedores');
+
+        Route::get('/produtos', function(){
+            return 'Produtos';
+        })->name('app.produtos');
+    });
+
+
+    Route::get('/rota1', function() {
+        echo 'Rota 1';
+    })->name('site.rota1');
+
+    // Route::get('/rota2', function() {
+    //     echo 'Rota 2';
+    // })->name('site.rota2');
+
+                // (origem, destino)
+    Route::redirect('/rota2', '/rota1');
 
