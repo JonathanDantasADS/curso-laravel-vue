48a575a3d431c4aab783a5f2112ef0b92b885854
JonathanDantasADS <jonathandantasadm@gmail.com>
2022-07-12 21:22:02 -0300

    Criando Aplicação Full Stack com Laravel e VueJS

--- routes/web.php
+++ routes/web.php
@@ -12,37 +12,14 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-    
-    Route::get('/', 'PrincipalController@principal')->name('site.index');
-    Route::get('/sobre-nos', 'SobreNosController@sobreNos')->name('site.sobrenos');
-    Route::get('/contato', 'ContatoController@contato')->name('site.contato');
-    Route::post('/contato', 'ContatoController@contato')->name('site.contato');
 
-    
-    Route::get('/login', function(){
-        return 'Login';
-    })->name('site.login');
+Route::get('/', function () {
+    return view('welcome');
+});
 
-    // Rotas agrupadas
-    Route::prefix('/app')->group(function(){
+Auth::routes();
 
-        Route::get('/clientes', function(){
-            return 'Clientes';
-        })->name('app.clientes');
-        
-        Route::get('/fornecedores', 'FornecedorController@index')->name('app.fornecedores');
-
-        Route::get('/produtos', function(){
-            return 'Produtos';
-        })->name('app.produtos');
-    });
-
-    Route::get('/teste/{p1}/{p2}','TesteController@teste')->name('teste');
-
-    // Rota de fallback, página personalizada caso o usuário digite o endereço errado
-    Route::fallback(function(){
-        echo 'A rota acessada não existe. 
-        <a href="'.route('site.index').'">Clique Aqui</a> 
-        para ir para a página inicial';
-    });
-
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/marcas', function() {
+    return view('app.marcas');
+})->name('marcas')->middleware('auth');
